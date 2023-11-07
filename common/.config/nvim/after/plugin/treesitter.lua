6d5f0cc0979706e05952310be27850252afb751c
Andrii Derhach <thequasar7@gmail.com>
2023-11-08 01:09:24 +0200

    Update treesitter config (no immediate changes)

--- common/.config/nvim/after/plugin/treesitter.lua
+++ common/.config/nvim/after/plugin/treesitter.lua
@@ -9,12 +9,21 @@ require 'nvim-treesitter.configs'.setup {
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
 
+    -- List of parsers to ignore installing (or "all")
+    -- ignore_install = { "javascript" },
+
     ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
     -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
     highlight = {
         -- `false` will disable the whole extension
         enable = true,
+
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        -- disable = { "c", "rust" },
 
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         disable = function(lang, buf)
