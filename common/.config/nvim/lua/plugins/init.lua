4e95dd2a930849691a80cf10138ac5466176faa3
Andrii Derhach <thequasar7@gmail.com>
2024-05-23 20:05:24 +0300

    Add tmux config and integrate with NeoVim
    Also update the plugins for NeoVim

--- common/.config/nvim/lua/plugins/init.lua
+++ common/.config/nvim/lua/plugins/init.lua
@@ -41,4 +41,21 @@ require("lazy").setup({
       -- refer to the configuration section below
     }
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  }
 })
